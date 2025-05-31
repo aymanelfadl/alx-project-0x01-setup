682976f625e34b23e03f0bf3e14dd04733827792
ayman elfadl <aymanelfadl16@gmail.com>
2025-05-31 09:23:04 +0100

    ok

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -50,7 +50,7 @@ const UserCard: React.FC<UserProps> = ({ user, expanded, onToggle }) => {
         onClick={onToggle}
         className="mt-4 text-blue-600 hover:text-blue-900 cursor-pointer font-semibold"
       >
-        {expanded ? "See less" : "See more"}
+        {expanded ? "": "show more"}
       </button>
     </div>
   );
--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -43,3 +43,15 @@ export interface UserProps {
     user?: UserCardProps;
     users?: UserCardProps[];
 }
+
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
